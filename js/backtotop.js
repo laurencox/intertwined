0295779e9c9250c777c444e183a266a980d0dfa4
laurencox <lauren.cox@montserrat.edu>
2017-05-11 16:11:14 -0400

    Add files via upload

---
+++ js/backtotop.js
@@ -0,0 +1,16 @@
+// From W3Schools
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top").style.display = "block";
+    } else {
+        document.getElementById("top").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
